a14256a8cd98b54beb4996aa8afe34afc0c9cfe1
{The BOSS} David Parra-Sandoval  A MAN!!!!!! <Parralogic@gmail.com>
2023-10-03 22:13:14 -0700

    Add files via upload

---
+++ READme.txt
@@ -0,0 +1,8 @@
+This script will secure your linux box/system/computer from unauthorized
+usage, by disabling sudo usage; your systemsettings and your bashrc. This script
+was written in Arch Linux with the plasma5 desktop environment.
+
+UPDATE! 10/03/2023
+To my knowledge, this script at the moment, will work IF your the ONLY user
+using your system and in the wheel group for the sudo command....
+still working on it......
---
+++ secure.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#Creator: David Parra-Sandoval
+#Date: 09/29/2023
+#Last Modified: 10/03/2023
+clear
+
+echo "Select My Idea of, Linux Security!"
+select OPTION in SECURE UNSECURE; do
+case $OPTION in
+SECURE)
+if [[ $UID = 0 ]]; then
+echo "running as root!!"
+echo "only run this selection as sudo user!"
+exit 1
+fi
+whoami > MySystemIsSECURED  
+THEUSER=`whoami`
+SUPERUSER=$(sudo cat /etc/sudoers | grep "wheel ALL=(ALL) ALL")
+REPLACE="# %wheel ALL=(ALL) ALL"
+sudo chmod 000 /usr/bin/systemsettings
+chmod 400 /home/$THEUSER/.bashrc
+sudo sed -i "s/$SUPERUSER/$REPLACE/" /etc/sudoers
+break
+;;
+UNSECURE)
+if [[ $UID != 0 ]]; then
+echo "not root!"
+exit 1
+fi
+until [[ $REPLY = [yY]* ]]; do
+echo "Select User to Unsecure!"
+select UUSER in $(ls /home); do
+USERTOUNSECURE=$UUSER
+read -p "UNSECURE $USERTOUNSECURE?[y/n] "
+case $REPLY in
+y|Y|Yes|YES) echo OK!; break
+;;
+n|N|No|NO) echo re-select!
+;;
+esac
+done
+done
+SUPERUSER=$(cat /etc/sudoers | grep "wheel ALL=(ALL) ALL")
+REPLACE=" %wheel ALL=(ALL) ALL"
+sed -i "s/$SUPERUSER/$REPLACE/" /etc/sudoers
+chmod 755 /usr/bin/systemsettings
+chmod 644 /home/$USERTOUNSECURE/.bashrc
+break
+;;
+esac
+done
+
+if [[ $OPTION = SECURE ]]; then
+echo "sudo privileges removed/onhold till UNSECURE is selected!"
+echo "systemsettings unavailable/unexecutable!"
+echo "your bashrc file is only readable!"
+echo "REMEMBER! run this script as root not sudo to UNSECURE it!"
+echo "SYSTEM SECURED" >> /home/$THEUSER/MySystemIsSECURED
+xmessage -nearmouse "SystemSecured! to Unsecure run su in terminal, you need the root password then execute secure.sh" 2> /dev/null  
+else
+echo "sudo privileges active/not-onhold!"
+echo "systemsettings available/executable!"
+echo "your bashrc file readable and writable!" 
+rm /home/david/MySystemIsSECURED &> /dev/null
+fi
+
